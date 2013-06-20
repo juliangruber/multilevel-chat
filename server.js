e11d6f3843637fa902c3289176ec221d3f1f05d2
Julian Gruber <julian@juliangruber.com>
2013-06-20 17:23:51 +0200

    added browserify

--- server.js
+++ server.js
@@ -4,12 +4,22 @@
 
 var http = require('http');
 var fs = require('fs');
+var browserify = require('browserify');
 
 var server = http.createServer(function (req, res) {
   if (req.url == '/') {
+
     fs.createReadStream(__dirname + '/index.html').pipe(res);
+
+  } else if (req.url == '/bundle.js') {
+
+    res.writeHead(200, { 'Content-Type': 'application/javascript' });
+    browserify('./client.js').bundle({ debug: true }).pipe(res);
+
   } else {
+
     res.end('oops');
+
   }
 });
 
